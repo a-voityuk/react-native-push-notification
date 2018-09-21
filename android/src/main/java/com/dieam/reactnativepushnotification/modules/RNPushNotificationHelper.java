ffaaf9019cfb31a47c93e66b2b758127ac2c181b
a.voityuk <a.voityuk@gmail.com>
2018-09-21 22:39:43 +0300

    fix compile

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -622,4 +622,69 @@ public class RNPushNotificationHelper {
         manager.createNotificationChannel(channel);
         channelCreated = true;
     }
+
+    public static Map<String, Object> jsonToMap(String t) throws JSONException {
+        if (t == null) return null;
+
+        Map<String, Object> retMap = new HashMap<>();
+        JSONObject json = new JSONObject(t);
+
+        if (json != JSONObject.NULL) {
+            retMap = toMap(json);
+        }
+
+        return retMap;
+    }
+
+    public static Map<String, Object> toMap(JSONObject object) throws JSONException {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        Iterator<String> keysItr = object.keys();
+        while(keysItr.hasNext()) {
+            String key = keysItr.next();
+            Object value = object.get(key);
+
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    public static List<Object> toList(JSONArray array) throws JSONException {
+        List<Object> list = new ArrayList<Object>();
+        for(int i = 0; i < array.length(); i++) {
+            Object value = array.get(i);
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }
+        return list;
+    }
+
+    private String getLocalizedStringResourceByKey(String key) {
+        String localizedText = "";
+
+        try {
+            String packageName = context.getPackageName();
+            Resources res = context.getResources();
+            int resId = res.getIdentifier(key, "string", packageName);
+
+            localizedText = res.getString(resId);
+        } catch (Exception e) {
+            Log.d(LOG_TAG, "Couldn't find string for push notification key");
+        }
+
+        return localizedText;
+    }
 }
